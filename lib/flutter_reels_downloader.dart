97fa55bf63828209688419525b8017822e082fdf
Harsh Chhikara <harshchhikara1993@gmail.com>
2021-08-30 21:37:59 +0530

    Some changes

--- lib/flutter_reels_downloader.dart
+++ lib/flutter_reels_downloader.dart
@@ -4,12 +4,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-class ReelDownloader{
+class ReelDownloader {
   String url = "https://www.instagram.com/";
 
   Future<String> downloadReels(String link) async {
     var linkEdit = link.replaceAll(" ", "").split("/");
-    var downloadURL = await http.get(Uri.parse('${linkEdit[0]}//${linkEdit[2]}/${linkEdit[3]}/${linkEdit[4]}' + "/?__a=1"));
+    var downloadURL = await http.get(Uri.parse(
+        '${linkEdit[0]}//${linkEdit[2]}/${linkEdit[3]}/${linkEdit[4]}' +
+            "/?__a=1"));
     var data = json.decode(downloadURL.body);
     var graphql = data['graphql'];
     var shortcodeMedia = graphql['shortcode_media'];
